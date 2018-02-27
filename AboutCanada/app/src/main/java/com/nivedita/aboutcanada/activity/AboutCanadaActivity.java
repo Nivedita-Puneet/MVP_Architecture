69d4eb3de3640e023a52aad30efbef59a543897f
Nivedita-puneet <nivedita.puneet@gmail.com>
2018-02-27 22:25:17 +1100

    set up project directory structure

---
+++ AboutCanada/app/src/main/java/com/nivedita/aboutcanada/activity/AboutCanadaActivity.java
@@ -0,0 +1,17 @@
+package com.nivedita.aboutcanada.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nivedita.aboutcanada.R;
+
+/*Created by Neetu 27-02-2018*/
+
+public class AboutCanadaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_canada);
+    }
+}
